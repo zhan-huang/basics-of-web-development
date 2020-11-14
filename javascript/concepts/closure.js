5e789ebd5b45f1f38cdeb4081c458a4db8d00b80
Zhan Huang <zhuang@ebi.ac.uk>
2020-11-14 12:21:46 +0000

    javascript/concepts/closures.js

--- javascript/concepts/closure.js
+++ javascript/concepts/closure.js
@@ -15,7 +15,7 @@ for (let i = 0; i < 10; i++) {
   setTimeout(function() {console.log(i)}, 100)
 }
 for (var i = 0; i < 10; i++) {
-  (function(j) {
+  (function(j = i) {
     setTimeout(function() {console.log(j)}, 100)
   })()
 }
