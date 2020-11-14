27e1c5d35a128b4f82bdc488bf24dfc4a803ba06
Zhan Huang <zhuang@ebi.ac.uk>
2020-11-14 12:18:43 +0000

    javascript/concepts/closure.js

---
+++ javascript/concepts/closure.js
@@ -0,0 +1,21 @@
+// create a function returning another function
+function add(x) {
+  return function(y) {
+    return x + y
+  }
+}
+var add5 = add(5)
+console.log(add5(3))
+
+// an example of the creation of a closure when a scope is created
+for (var i = 0; i < 10; i++) {
+  setTimeout(function() {console.log(i)}, 100)
+}
+for (let i = 0; i < 10; i++) {
+  setTimeout(function() {console.log(i)}, 100)
+}
+for (var i = 0; i < 10; i++) {
+  (function(j) {
+    setTimeout(function() {console.log(j)}, 100)
+  })()
+}
