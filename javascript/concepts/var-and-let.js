6a3fa7cb53d039e3aa7f178300b8ffb7f9032de4
Zhan Huang <zhuang@ebi.ac.uk>
2020-11-17 09:41:21 +0000

    javascript/concepts/var-and-let.js

---
+++ javascript/concepts/var-and-let.js
@@ -0,0 +1,35 @@
+// var is function scope (will be hoisted on top), while let is block scope
+function f() {
+  console.log(a)
+  var a = 0
+}
+f()
+
+function f2() {
+  try {
+    console.log(a)
+    let a = 0
+  } catch(e) {
+    console.log('failed')
+  }
+}
+f2()
+
+// var will be rewriten in the same function scope
+function f3() {
+  var b = 0
+  if (true) {
+    var b = 1
+  }
+  console.log(b)
+}
+f3()
+
+function f4() {
+  let b = 0
+  if (true) {
+    let b = 1
+  }
+  console.log(b)
+}
+f4()
