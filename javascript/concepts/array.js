e445033db807a20458255cd4614323629783da19
Zhan Huang <zhuang@ebi.ac.uk>
2020-11-25 15:09:09 +0000

    array

---
+++ javascript/concepts/array.js
@@ -0,0 +1,12 @@
+var arr = [1, 2, 3, 4]
+
+// destructuring assignment
+var [a, b] = arr
+console.log(a, b)
+var [b, a] = [a, b]
+console.log(a, b)
+
+// spread operator
+console.log(...arr)
+arr = [...arr, 5, 6, 7]
+console.log(...arr)
